77d16e58c8c11b0ea623c39a3b107561854fd025
abhilash148 <saiabhilash148@gmail.com>
2024-08-31 14:34:54 -0500

    Initial Commit

---
+++ BikerUITests/BikerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BikerUITestsLaunchTests.swift
+//  BikerUITests
+//
+//  Created by Sai Abhilash Gudavalli on 31/08/24.
+//
+
+import XCTest
+
+final class BikerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
