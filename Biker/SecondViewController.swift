4121cdb0f9530684b1e44ae54c8aed84e9528d4d
abhilash148 <saiabhilash148@gmail.com>
2024-08-31 21:16:39 -0500

    08-31-2024: Tab bar screens created

--- Biker/SecondViewController.swift
+++ Biker/SecondViewController.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+
+class SecondViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeVC = HomeViewController()
+        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 0)
+        
+        let profileVC = ProfileViewController()
+        profileVC.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 1)
+        
+        let viewControllersList = [homeVC, profileVC]
+        
+        viewControllers = viewControllersList
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.view.backgroundColor = .white
+        
+        
+        self.navigationItem.setHidesBackButton(true, animated: true)
+    }
+    
+    
+}
--- Biker/ViewController.swift
+++ Biker/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBAction func onClickNext(_ sender: UIButton) {
+        
+        print("Next button clicked")
+        
+        let secondVC = self.storyboard?.instantiateViewController(identifier: "SecondVC") as! SecondViewController
+        
+        self.navigationController?.pushViewController(secondVC, animated: true)
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.view.backgroundColor = .white
     }
 
 
