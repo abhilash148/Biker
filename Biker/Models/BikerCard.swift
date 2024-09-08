ae0176130ca72b5a1f4cfdbc88cd70c8f0d242a0
abhilash148 <saiabhilash148@gmail.com>
2024-09-08 17:14:06 -0500

    09-08-2024: Updated Home with TableView

---
+++ Biker/Models/BikerCard.swift
@@ -0,0 +1,20 @@
+//
+//  BikerCard.swift
+//  Biker
+//
+//  Created by Sai Abhilash Gudavalli on 08/09/24.
+//
+
+import Foundation
+
+struct BikerCard {
+    
+    var name: String
+    var number: String
+    
+    init(name: String, number: String) {
+        self.name = name
+        self.number = number
+    }
+    
+}
--- Biker/ViewControllers/HomeViewController.swift
+++ Biker/ViewControllers/HomeViewController.swift
@@ -11,11 +11,28 @@ import UIKit
 class HomeViewController: UIViewController {
     
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+}
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
     
 }
